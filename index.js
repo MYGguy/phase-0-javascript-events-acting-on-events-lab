7c4ac126602d78859646a96824334f325391a197
Moshe Goldstein <mygguy613@gmail.com>
2024-07-19 18:47:49 -0400

    ct

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Your code here
+const dodger = document.getElementById('dodger');
+
+function dodgerPosition() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    return left;
+}
+
+function moveDodgerLeft(){
+    const left = dodgerPosition();
+        if (left > 0) {
+            dodger.style.left = `${left - 1}px`;
+        }
+}
+
+function moveDodgerRight() {
+    const left = dodgerPosition();
+    
+    if (left < 360) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
+
+document.addEventListener('keydown', function (e) {
+    if (e.key === 'ArrowLeft') {
+    moveDodgerLeft();   
+    }
+})
+
+document.addEventListener('keydown', function (e) {
+    if (e.key === 'ArrowRight') {
+        moveDodgerRight();
+    }
+})
+
